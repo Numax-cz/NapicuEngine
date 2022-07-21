f6b55d57fcf49c18b07e84546d42188fad28c302
Marcel <numax.cz@protonmail.ch>
2022-07-21 19:46:31 +0200

    První commit

---
+++ CMakeFiles/OpenGL.dir/src/main.c.obj.d
@@ -0,0 +1,21 @@
+CMakeFiles/OpenGL.dir/src/main.c.obj: D:\Dokumenty\OpenGL\src\main.c \
+ D:/Apps/MinGW/mingw64/x86_64-w64-mingw32/include/stdio.h \
+ D:/Apps/MinGW/mingw64/x86_64-w64-mingw32/include/crtdefs.h \
+ D:/Apps/MinGW/mingw64/x86_64-w64-mingw32/include/_mingw.h \
+ D:/Apps/MinGW/mingw64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ D:/Apps/MinGW/mingw64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ D:/Apps/MinGW/mingw64/x86_64-w64-mingw32/include/vadefs.h \
+ D:/Apps/MinGW/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_directx.h \
+ D:/Apps/MinGW/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ D:/Apps/MinGW/mingw64/x86_64-w64-mingw32/include/_mingw_print_push.h \
+ D:/Apps/MinGW/mingw64/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ D:/Apps/MinGW/mingw64/x86_64-w64-mingw32/include/swprintf.inl \
+ D:/Apps/MinGW/mingw64/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ D:/Apps/MinGW/mingw64/x86_64-w64-mingw32/include/_mingw_print_pop.h \
+ D:\Dokumenty\OpenGL\src\window.h \
+ D:/Dokumenty/OpenGL/glfw/include/GLFW/glfw3.h \
+ D:/Apps/MinGW/mingw64/lib/gcc/x86_64-w64-mingw32/8.1.0/include/stddef.h \
+ D:/Apps/MinGW/mingw64/x86_64-w64-mingw32/include/stddef.h \
+ D:/Apps/MinGW/mingw64/lib/gcc/x86_64-w64-mingw32/8.1.0/include/stdint.h \
+ D:/Apps/MinGW/mingw64/x86_64-w64-mingw32/include/stdint.h \
+ D:/Apps/MinGW/mingw64/x86_64-w64-mingw32/include/GL/gl.h
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "window.h"
+
+int main() {
+
+    window_init(1280, 720, "OpenGLWindow");
+
+    return 0;
+}
