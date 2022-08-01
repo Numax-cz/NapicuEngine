68e711f84bc37fdc6767a723caac17c74716e219
Marcel <numax.cz@protonmail.ch>
2022-08-01 22:04:30 +0200

    Upravena kamera

--- src/Engine/Camera.cpp
+++ src/Engine/Camera.cpp
@@ -3,4 +3,19 @@
 #include "Camera.h"
 namespace Napicu{
 
+    Camera::Camera(Vector2f position) {
+        this->position = position;
+        this->pMatrix = *new Matrix4f();
+        this->vMatrix = *new Matrix4f();
+    }
+
+    void Camera::pAdjust() {
+        this->pMatrix.Identity();
+        this->pMatrix.Orthographic(0.0f, 32.0f * 40.0f, 0.0f, 32.0f * 21.0f, 0.0f, 100.0f);
+    }
+
+    Matrix4f Camera::getVMatrix() {
+        this->vMatrix = Matrix4f::Translate(this->position);
+
+    }
 }
--- src/Engine/Camera.h
+++ src/Engine/Camera.h
@@ -3,14 +3,21 @@
 #ifndef OPENGL_CAMERA_H
 #define OPENGL_CAMERA_H
 
+#include "Math/Vector2f.h"
+#include "Math/Matrix4f.h"
 
 namespace Napicu{
     class Camera {
-
     private:
+        //projection - view
+        Matrix4f pMatrix;
+        Matrix4f vMatrix;
+        Vector2f position;
 
     public:
-
+        Camera(Vector2f position);
+        Matrix4f getVMatrix();
+        void pAdjust();
 
     };
 
