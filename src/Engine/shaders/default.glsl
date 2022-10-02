1633e32c236e87e0508abd30286e5a3433cdfee8
Marcel <numax.cz@protonmail.ch>
2022-10-02 16:15:25 +0200

    Opraveny shadery

--- src/Engine/shaders/default.glsl
+++ src/Engine/shaders/default.glsl
@@ -3,13 +3,13 @@
 #version 460 core
 
 layout (location=0) in vec3 aPos;
-layout (location=1) in vec4 aColor;
+layout (location=1) in vec3 aColor;
 layout (location=2) in vec2 aTexCords;
 
 uniform mat4 uProjection;
 uniform mat4 uView;
 
-out vec4 fColor;
+out vec3 fColor;
 out vec2 vTexCords;
 
 void main() {
