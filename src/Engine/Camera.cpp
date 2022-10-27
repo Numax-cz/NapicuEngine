a4b29e75f9943074a0ec07982b92da27f084b055
Marcel <numax.cz@protonmail.ch>
2022-10-27 10:38:11 +0200

    Upravena kamera

--- src/Engine/Camera.cpp
+++ src/Engine/Camera.cpp
@@ -4,11 +4,29 @@
 namespace Napicu {
     Camera::Camera(float left, float right, float bottom, float top)
             : pMatrix(glm::ortho(left, right, bottom, top, 0.0f, 100.0f)), vMatrix(1.0f) {
+
     }
 
-    void Camera::recalculateVMatrix() {
-        glm::mat4 tr = glm::translate(glm::mat4(1.0f), this->position)
-                       * glm::rotate(glm::mat4(1.0f), this->rotation, glm::vec3(0, 0, -1.0f));
-        this->vMatrix = glm::inverse(tr);
+    glm::mat4 Camera::getViewMatrix() {
+        glm::vec3 front = * new glm::vec3(0.0f, 0.0f, -1.0f);
+        glm::vec3 up = * new glm::vec3(0.0f, 1.0f, 0.0f);
+
+        //this->vMatrix = * new glm::mat4(1.0f);
+
+        this->vMatrix = glm::lookAt(position, position + front, up);
+
+
+
+        return this->vMatrix;
     }
+
+    glm::mat4 Camera::getProjectionMatrix() {
+        return this->pMatrix;
+    }
+
+    glm::mat4 Camera::getViewProjectionMatrix() {
+        return this->getViewMatrix() * this->getProjectionMatrix();
+    }
+
+
 }
--- src/Engine/Camera.h
+++ src/Engine/Camera.h
@@ -9,36 +9,22 @@ namespace Napicu {
     class Camera {
     private:
         //projection - view
-        glm::mat4 pMatrix;
-        glm::mat4 vMatrix;
-        glm::vec3 position;
-        float rotation;
+        glm::mat4 pMatrix, vMatrix;
+        glm::vec3 position = *new glm::vec3(0.0f, 0.0f, 0.0f);
+
+       // float rotation;
 
 
 
     public:
         Camera(float left, float right, float bottom, float top);
-        void recalculateVMatrix();
 
-        void setPosition(const glm::vec3 &position) {
-            this->position = position;
-            this->recalculateVMatrix();
-        }
+        glm::mat4 getViewMatrix();
 
-        void setRotation(float angle) {
-            this->rotation = rotation;
-            this->recalculateVMatrix();
-        }
+        glm::mat4 getProjectionMatrix();
 
-        const glm::mat4 &getProjectionMatrix() const { return this->pMatrix; }
+        glm::mat4 getViewProjectionMatrix();
 
-        const glm::mat4 &getViewMatrix() const { return this->vMatrix; }
-
-        const glm::mat4 getViewProjectionMatrix() const { return this->vMatrix * this->pMatrix; }
-
-        const float getRotation() const { return this->rotation; }
-
-        const glm::vec3 getPosition() const { return this->position; }
 
     };
 }
