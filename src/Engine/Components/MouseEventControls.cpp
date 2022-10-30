4b0a3da19063105e9a57b0e151407567118ddec2
Marcel <numax.cz@protonmail.ch>
2022-10-30 16:27:49 +0100

    Upraven MouseEvent

--- src/Engine/Components/MouseEventControls.cpp
+++ src/Engine/Components/MouseEventControls.cpp
@@ -6,22 +6,26 @@ namespace Napicu{
 
     void MouseEventControls::pickUpObject(Napicu::Object *object) {
         this->holdingObject = object;
-        Window::current_scene->addObjectToScene(object);
+        object->transform->position = new glm::vec2(600, 600);
     }
 
+
     void MouseEventControls::drop() {
+        Window::current_scene->addObjectToScene(this->holdingObject);
         this->holdingObject = nullptr;
+
     }
 
     void MouseEventControls::update(double delta_time) {
         if(this->holdingObject != nullptr){
-
-            this->holdingObject->transform->position.x = Napicu::MouseEvent::getOrthoX() - 16;
-            this->holdingObject->transform->position.y = Napicu::MouseEvent::getOrthoY() - 16;
+//
+            this->holdingObject->transform->position->x = Napicu::MouseEvent::getOrthoX() - 16;
+            this->holdingObject->transform->position->y = Napicu::MouseEvent::getOrthoY() - 16;
 
 
             if(Napicu::MouseEvent::mouseButtonDown(GLFW_MOUSE_BUTTON_LEFT)){
-                //this->drop();
+                this->drop();
+
             }
 
         }
