5921291ec22ac99782426467f96863723d80ada2
Marcel <numax.cz@protonmail.ch>
2022-11-17 10:30:19 +0100

    Upravena správa dat

--- src/Engine/Utils/DataManagment.cpp
+++ src/Engine/Utils/DataManagment.cpp
@@ -7,17 +7,36 @@ namespace Napicu {
     void DataManagment::loadData() {
         nlohmann::json* j = new nlohmann::json();
         std::ifstream i(DataManagment::DATA_FILE_NAME);
-        if(i){
-            i >> *j;
-            DataManagment::data = j;
+
+        if(i.is_open()){
+            Napicu::JsonParseException jsonParseException(*j);
+            nlohmann::json::sax_parse(i, &jsonParseException);
         }
         else {
             std::ofstream o(DataManagment::DATA_FILE_NAME);
+            //TODO WRITE DEFAULT VALUE
+            o.close();
+        }
+        DataManagment::data = j;
+        i.close();
+    }
+
+    void DataManagment::saveData() {
+        if(DataManagment::data == nullptr) return;
+        std::ofstream o(DataManagment::DATA_FILE_NAME);
+        if(o.is_open()){
+            o << *DataManagment::data;
             o.close();
         }
     }
 
     nlohmann::json* DataManagment::getData() {
         return DataManagment::data;
+    }
+
+    void DataManagment::saveObjectToData(Napicu::Object ob) {
+        if(DataManagment::data == nullptr) return;
+        (*DataManagment::data)["objects"].push_back(Napicu::Json::objectToJson(ob));
+        DataManagment::saveData();
     }
 }
--- src/Engine/Utils/DataManagment.h
+++ src/Engine/Utils/DataManagment.h
@@ -1,5 +1,9 @@
 #pragma once
 #include <nlohmann/json.hpp>
+#include "Json.h"
+#include "JsonParse.h"
+#include "../Object/Object.h"
+
 
 namespace Napicu {
 
@@ -7,9 +11,14 @@ namespace Napicu {
 
     private:
         static inline const std::string DATA_FILE_NAME = "data.json";
+
         inline static nlohmann::json* data = nullptr;
 
+        static void saveData();
+
+
     public:
+        static void saveObjectToData(Napicu::Object ob);
 
         static void loadData();
 
