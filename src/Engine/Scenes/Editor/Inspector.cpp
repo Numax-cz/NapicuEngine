ede0ec58b2004e3d873cba1965c27e7bcc66b539
Marcel <numax.cz@protonmail.ch>
2022-12-07 16:20:49 +0100

    Uprava

--- src/Engine/Scenes/Editor/Inspector.cpp
+++ src/Engine/Scenes/Editor/Inspector.cpp
@@ -1,17 +1,12 @@
 #include "Inspector.h"
 #include <imgui.h>
 #include "../../Events/MouseEvent.h"
-#include "../../window.h"
-
 
 namespace Napicu{
-
 
     Inspector::Inspector(Napicu::Select* selectedTexture)
             : selectedTexture(selectedTexture) {
     }
-
-
 
     void Inspector::imgui() {
         if (this->activeGameObject != nullptr) {
@@ -22,7 +17,6 @@ namespace Napicu{
     }
 
     void Inspector::update(float delta_time, Scene *scene) {
-
         Napicu::MouseEvent::mouseButtonDownEvent(GLFW_MOUSE_BUTTON_LEFT, [scene, this]() {
             int y = (int) Napicu::MouseEvent::getScreenY();
             int x = (int) Napicu::MouseEvent::getScreenX();
@@ -31,11 +25,5 @@ namespace Napicu{
                 this->activeGameObject = scene->getSceneObject(this->selectedTexture->read(x, y));
             }
         });
-
-
-
     }
-
-
-
 }
