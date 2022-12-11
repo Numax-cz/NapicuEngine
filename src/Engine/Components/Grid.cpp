f79aa84ebfda51cfaed491b02243054053e74d81
Marcel <numax.cz@protonmail.ch>
2022-12-11 10:54:31 +0100

    Upraven grid

--- src/Engine/Components/Grid.cpp
+++ src/Engine/Components/Grid.cpp
@@ -12,8 +12,11 @@ namespace Napicu{
         int fX = ((int)(cameraPosition.x / Napicu::Config::GRID_WIDTH ) - 1) * Napicu::Config::GRID_HEIGHT;
         int fY = ((int)(cameraPosition.y / Napicu::Config::GRID_HEIGHT) - 1) * Napicu::Config::GRID_HEIGHT;
 
-        int numVerticalLines = (int)(Napicu::Config::CAMERA_PROJECTION_SIZE_X / Napicu::Config::GRID_WIDTH) + 2;
-        int numHorizontalLines = (int)(Napicu::Config::CAMERA_PROJECTION_SIZE_Y / Napicu::Config::GRID_HEIGHT) + 2;
+        int numVerticalLines = (int)((Napicu::Config::CAMERA_PROJECTION_SIZE_X * Napicu::Window::current_scene->getCamera()->getZoomLevel()) / Napicu::Config::GRID_WIDTH) + 2;
+        int numHorizontalLines = (int)((Napicu::Config::CAMERA_PROJECTION_SIZE_Y * Napicu::Window::current_scene->getCamera()->getZoomLevel()) / Napicu::Config::GRID_HEIGHT) + 2;
+
+        int height = (int)(Napicu::Config::CAMERA_PROJECTION_SIZE_Y  * Napicu::Window::current_scene->getCamera()->getZoomLevel()) * Napicu::Config::GRID_HEIGHT * 2;
+        int width = (int)(Napicu::Config::CAMERA_PROJECTION_SIZE_X  * Napicu::Window::current_scene->getCamera()->getZoomLevel()) + Napicu::Config::GRID_WIDTH * 2;
 
         int maxLines = std::max(numVerticalLines, numHorizontalLines);
 
@@ -23,12 +26,12 @@ namespace Napicu{
 
             if(i < numVerticalLines){
                 Napicu::Draw::addLine(glm::vec2(x, fY),
-                                      glm::vec2(x, fY + (Napicu::Config::CAMERA_PROJECTION_SIZE_Y + Napicu::Config::CAMERA_PROJECTION_SIZE_Y * 2)));
+                                      glm::vec2(x, fY + height));
             }
 
             if(i < numHorizontalLines){
                 Napicu::Draw::addLine(glm::vec2(fX, y),
-                                      glm::vec2(fX + (Napicu::Config::CAMERA_PROJECTION_SIZE_X + Napicu::Config::CAMERA_PROJECTION_SIZE_X  * 2), y));
+                                      glm::vec2(fX + width, y));
             }
         }
 
